3a84e5bd3b5022a9be8e103fe93f6616b10c1b7e
Ericn7 <ericng@globaltrack.my>
2022-07-30 23:59:49 +0800

    Add flex

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className='flex h-screen w-full justify-center items-center gap-4'>
+    <div className='flex flex-col h-screen w-screen justify-center items-center gap-4'>
       <button className='bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded text-2xl font-semibold'>Hello World</button>
       <button className='bg-purple-500 hover:bg-purple-700 text-white py-2 px-4 rounded text-2xl font-semibold'>Hello World</button>
       <button className='bg-gray-500 hover:bg-gray-700 text-white py-2 px-4 rounded text-2xl font-semibold'>Hello World</button>
